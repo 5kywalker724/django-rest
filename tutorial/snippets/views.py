dd90e1b5c8d131232e55699c38a7edfd30c969d0
PinT271 <olezhka.kuzmin.99@inbox.ru>
2023-11-21 17:37:24 +0700

    Добавление представлений для юзера

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -12,3 +13,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
